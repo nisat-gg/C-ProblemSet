2b264686a7fe8c21540aa4d8dfb1ac13fc21dcc1
nisat-gg <nisathasan13@gmail.com>
2024-12-24 03:47:43 +0600

    n

---
+++ ExamProb (CodeforWin)/string/wordcount.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#define MAX_SIZE 100 
+
+int main()
+{
+    char str[MAX_SIZE];
+    int i, words;
+
+
+    gets(str);
+
+    i = 0;
+    words = 1;
+
+
+    while(str[i] != '\0')
+    {
+      
+        if(str[i]==' ' || str[i]=='\n' || str[i]=='\t')
+        {
+            words++;
+        }
+
+        i++;
+    }
+
+    printf("Total number of words = %d", words);
+
+    return 0;
+}
